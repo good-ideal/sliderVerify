1ef7bc32dc2ca8964b89cd1226e772bca5db3f9d
理想三旬 <tiandikv@163.com>
2019-01-16 10:25:56 +0800

    完善

---
+++ README.md
@@ -0,0 +1,87 @@
+## 滑块验证
+没错，闲暇时光玩玩小东西，此插件仅提供学习交流
+## 快速上手
+
+获得 layui 后，将其完整地部署到你的项目目录（或静态资源服务器），你只需要引入下述一个文件：
+
+```
+./sliderVerify/sliderVerify.js
+```
+
+入门案例：
+
+```html
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+		<meta name="renderer" content="webkit">
+	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="stylesheet" href="layui/css/layui.css" />
+		</head>
+		<body>
+			<form class="layui-form" action="">
+				<div class="layui-form-item">
+					<label class="layui-form-label">输入框</label>
+					<div class="layui-input-block">
+						<input type="text" name="title" required lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+					</div>
+				</div>
+				<div class="layui-form-item">
+					<label class="layui-form-label">滑动验证</label>
+					<div class="layui-input-block">
+						<div id="slider"></div>
+					</div>
+				</div>
+				<div class="layui-form-item">
+					<div class="layui-input-block">
+						<button class="layui-btn" lay-submit lay-filter="formDemo">立即提交</button>
+						<button type="reset" class="layui-btn layui-btn-primary">重置</button>
+					</div>
+				</div>
+			</form>
+<script src="../layui/layui.js"></script>
+<script>
+		//一般直接写在一个js文件中
+		layui.config({
+			base: 'dist/sliderVerify/'
+		}).use(['sliderVerify', 'jquery', 'form'], function() {
+			var sliderVerify = layui.sliderVerify,
+				form = layui.form;
+			var slider = sliderVerify.render({
+				elem: '#slider',
+				onOk: function(){//当验证通过回调
+					layer.msg("滑块验证通过");
+				}
+			})
+			//监听提交
+			form.on('submit(formDemo)', function(data) {
+				if(slider.isOk()){//用于表单验证是否已经滑动成功
+					layer.msg(JSON.stringify(data.field));
+				}else{
+					layer.msg("请先通过滑块验证");
+				}
+				return false;
+			});
+			
+		})
+</script> 
+</body>
+</html>
+```
+
+如果你想按照自己的风格定义样式你可以这样：
+
+```js
+	var slider = sliderVerify.render({
+		elem: '#slider',
+		isAutoVerify:false,//关闭自动验证
+		bg : 'layui-',//自定义背景样式名
+		text : '滑动',
+		onOk: function(){//当验证通过回调
+			layer.msg("滑块验证通过");
+		}
+	})
+```
--- src/sliderVerify/sliderVerify.js
+++ src/sliderVerify/sliderVerify.js
@@ -11,7 +11,7 @@ layui.define(['jquery','layer','form'], function (exports) {
   	device = layui.device()
   	,sliderVerify = {
 	  	read : function(){
-	  		var css = `.slider-item{height:40px;line-height:40px;background-color:#e2e2e2;position:relative}.slider-bg{position:absolute;width:40px;height:100%;z-index:100}.slider-bg-success{background-color:#5fb878}.slider-btn{width:40px;height:96%;position:absolute;border:1px solid #ccc;cursor:move;text-align:center;background-color:#fff;user-select:none;color:#666;z-index:120}.slider-btn-success{font-size:26px}.slider-text{position:absolute;text-align:center;width:100%;height:100%;user-select:none;font-size:14px;z-index:120}.slider-text-init{color:#6d6d6d}.slider-text-success{color:#fff}.slider-error{animation:glow 800ms ease-out infinite alternate;border:0 solid;border:1px solid}@keyframes glow{0%{border-color:#e6e6e6}100%{border-color:#ff5722}}`,
+	  		var css = `.slider-item{height:40px;line-height:40px;background-color:#d0d0d0;position:relative}.slider-bg{position:absolute;width:40px;height:100%;z-index:100}.slider-btn{width:40px;height:96%;position:absolute;border:1px solid #ccc;cursor:move;text-align:center;background-color:#fff;user-select:none;color:#666;z-index:120}.slider-btn-success{font-size:26px}.slider-text{position:absolute;text-align:center;width:100%;height:100%;user-select:none;font-size:14px;color:#fff;z-index:120}.slider-error{animation:glow 800ms ease-out infinite alternate;border:1px solid}@keyframes glow{0%{border-color:#e6e6e6}100%{border-color:#ff5722}}`,
 			style = document.createElement('style');
 			style.innerHTML = css;
 			style.type = 'text/css';
@@ -41,6 +41,7 @@ layui.define(['jquery','layer','form'], function (exports) {
   	MOD_DEFAULT_BG = 'layui-bg-green',
   	MOD_ERROR_BORDER = 'slider-error',
   	MOD_CONFIG_TEXT = '请拖动滑块验证',
+  	MOD_CONFIG_SUCCESS = '验证通过',
 
   	
   	Class = function(option) {
@@ -66,9 +67,9 @@ layui.define(['jquery','layer','form'], function (exports) {
   		option.domid = that.createIdNum();
   		var sliderDom = $(`<div id="${option.domid}" ${option.isAutoVerify ? 'lay-verify="sliderVerify"' : ''} class="slider-item">
   								<div class="${MOD_BG} ${option.bg}"></div>
-  								<div class="${MOD_TEXT} ${MOD_TEXT}-init">${option.text}</div>
+  								<div class="${MOD_TEXT}">${option.text}</div>
   								<div class="${MOD_BTN} layui-icon layui-icon-next"></div>
-  							</div>`)
+  							</div>`);
   		elem.hide().after(sliderDom);
   		option.domid = $('#'+option.domid);
   		
@@ -87,7 +88,7 @@ layui.define(['jquery','layer','form'], function (exports) {
   	};
   	Class.prototype.isMobile = function(){
   		return (device.os == 'ios' || device.os == 'android');
-  	}
+  	};
   	Class.prototype.createIdNum = function(){
   		return MOD_NAME+(+new Date()).toString()+(Math.random().toString()).substr(2,7);
   	};
@@ -128,33 +129,32 @@ layui.define(['jquery','layer','form'], function (exports) {
   			//pc移动
   			document.onmousemove = move;
   		}
-  	}
+  	};
   	//移动
   	Class.prototype.move = function(down,e){
   		var that = this,
   		option = that.config,
   		container = option.container;
   		var e = e || window.event;
-        //1.获取鼠标移动后的水平位置
+        //鼠标移动后的水平位置
         let moveX = e.clientX || e.touches[0].clientX;
-        //2.得到鼠标水平位置的偏移量（鼠标移动时的位置 - 鼠标按下时的位置）
+        //鼠标水平位置的偏移量（鼠标移动时的位置 - 鼠标按下时的位置）
         var offsetX = moveX - down;
 
-        //3.在这里判断一下：鼠标水平移动的距离 与 滑动成功的距离 之间的关系
+        //判断一下：鼠标水平移动的距离 与 滑动成功的距离 之间的关系
         if( offsetX > container.distance ){
             offsetX = container.distance;//如果滑过了终点，就将它停留在终点位置
         }else if( offsetX < 0 ){
-            offsetX = 0;//如果滑到了起点的左侧，就将它重置为起点位置
+            offsetX = 0;//滑到了起点的左侧，将它重置为起点位置
         }
 		container.btn.style.left = offsetX + "px";
 		container.bg.style.width = offsetX + "px";
 		
-		//如果鼠标的水平移动距离 = 滑动成功的宽度
+		//鼠标的水平移动距离 = 滑动成功的宽度
         if( offsetX == container.distance ){
         	
-            //1.设置滑动成功后的样式
-            container.text.innerHTML = "验证通过";
-            container.text.style.color = "#fff";
+            //1.滑动成功后的样式
+            container.text.innerHTML = MOD_CONFIG_SUCCESS;
 			var com = window.getComputedStyle ? window.getComputedStyle(container.bg,null) : container.bg.currentStyle;
 			container.btn.style.border = '1px solid '+com.backgroundColor;
 			container.btn.style.color = com.backgroundColor;
@@ -182,7 +182,7 @@ layui.define(['jquery','layer','form'], function (exports) {
         }else{
         	document.onmouseup = seup;
         }
-  	}
+  	};
   	//放开
   	Class.prototype.stop = function(e){
   		var that = this,
@@ -197,14 +197,14 @@ layui.define(['jquery','layer','form'], function (exports) {
             container.btn.style.transition = "left 1s ease";
             container.bg.style.transition = "width 1s ease";
         }
-        //鼠标松开了，此时不需要拖动就清除鼠标移动和松开事件。
+        //鼠标松开了，不需要拖动就清除鼠标移动和松开事件。
         document.onmousemove = null;
         document.onmouseup = null;
          if( that.isMobile() ){
         	document.removeEventListener('touchmove',that.events.move,false);
         	document.removeEventListener('touchend',that.events.seup,false);
         }
-    }
+    };
   	//事件
   	Class.prototype.events = function() {
   		var that = this,
@@ -219,7 +219,7 @@ layui.define(['jquery','layer','form'], function (exports) {
   			btn : dom(btn),
   			bg  : dom(bg),
   			text: dom(text)
-  		}
+  		};
   		var distance = container.box.offsetWidth - container.btn.offsetWidth;//滑动成功的宽度（距离）
   		container.distance = distance;
   		option.container = container;
@@ -227,7 +227,7 @@ layui.define(['jquery','layer','form'], function (exports) {
 		that.events.down = down;
 		
 		if( that.isMobile() ){
-            container.btn.addEventListener('touchstart', that.events.down)	
+            container.btn.addEventListener('touchstart', that.events.down);	
         }else{
         	container.btn.onmousedown = down;
         }
@@ -236,7 +236,7 @@ layui.define(['jquery','layer','form'], function (exports) {
   	sliderVerify.render = function(option) {
   		var inst = new Class(option);
   		return thisSliderVerify.call(inst);
-  	}
+  	};
   	
   	exports(MOD_NAME, sliderVerify);
 })
